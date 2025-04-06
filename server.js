d1f4ee4a953825127167b6b1d76eef05989f30fe
Partha Maity <parthamaity2004@gmail.com>
2025-04-07 00:32:03 +0530

    start the server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+// require('dotenv').config();
+
+const bodyParser= require('body-parser');
+app.use(bodyParser.json()); // req body
+
+const PORT=process.env.PORT || 3000;
+
+app.listen(3000)
